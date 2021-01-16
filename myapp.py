2168f1f537cf0331eaf14bce022df60479502b51
chan <ssage7772@naver.com>
2021-01-16 22:24:20 +0900

    myapp html-1

--- myapp.py
+++ myapp.py
@@ -150,12 +150,11 @@ def save_query_by_parameters(parameters):
     return u"save {}".format(parameters.get('databaseEntry'))
 
 #웹페이지에 정답을 firebase에 저장
+@myapp.route("/post", methods=['POST'])
 def save_query_html():
     value = request.form['input']
-
-    data = db.collection(u'dialogflow')
-    data.add({u'정답': value})
-    return print(value)
+    msg = "%s 정답입니다." %value
+    return msg
 
 if __name__ == "__main__":
     # ssl_context = ssl.SSLContext(ssl.PROTOCOL_TLS)
